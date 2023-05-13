c7e737069bc5f7c579e966e330a7d3dd7a06e698
annakhv <annakhvedelidze@gmail.com>
2023-05-13 13:25:54 +0400

    implements generateTemplate method

--- src/main/java/com/messenger/template/TemplateGenerator.java
+++ src/main/java/com/messenger/template/TemplateGenerator.java
@@ -3,9 +3,14 @@ package com.messenger.template;
 import java.util.List;
 
 public class TemplateGenerator {
-  public static String  generateTemplate(List<String> values){
-      return null;
-  }
+    public static String generateTemplate(List<String> values) {
+        String value = "#{value}";
+        String template = "";
+        for (String val : values) {
+            template += val + " : " + value + "\n";
+        }
+        return template;
+    }
 
 
 }
