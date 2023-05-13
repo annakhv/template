f0f6ca45e0429e69d25e721f66641c6433dfaa98
annakhv <annakhvedelidze@gmail.com>
2023-05-13 13:50:54 +0400

    parseTemplateWithValid input test passes

--- src/main/java/com/messenger/template/Parser.java
+++ src/main/java/com/messenger/template/Parser.java
@@ -1,16 +1,24 @@
 package com.messenger.template;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class Parser {
     public static Map<String,String> parseTemplate(String template){
         String valueTemplate="#{value}";
       String[] values=template.split("\\n");
+      Map<String,String> map=new LinkedHashMap<>();
       for (String val:values){
           if (val.contains(valueTemplate)){
               throw new RuntimeException("template fields should not be empty");
+          }else{
+              String[] pair=val.split(":");
+              String key=pair[0].trim();
+              String value=pair[1].trim();
+              String endValue=value.substring(2,value.length()-1);
+              map.put(key,endValue);
           }
       }
-      return null;
+      return map;
     }
 }
