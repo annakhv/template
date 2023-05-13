15c7c871f84ddee9659efa75b8d88dd847dc3a87
annakhv <annakhvedelidze@gmail.com>
2023-05-13 13:43:29 +0400

    templateParseWithInvalidTemplateInput test passes

--- src/main/java/com/messenger/template/Parser.java
+++ src/main/java/com/messenger/template/Parser.java
@@ -4,6 +4,13 @@ import java.util.Map;
 
 public class Parser {
     public static Map<String,String> parseTemplate(String template){
-        return null;
+        String valueTemplate="#{value}";
+      String[] values=template.split("\\n");
+      for (String val:values){
+          if (val.contains(valueTemplate)){
+              throw new RuntimeException("template fields should not be empty");
+          }
+      }
+      return null;
     }
 }
