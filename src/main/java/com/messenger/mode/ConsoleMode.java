68e99e080187a355b0fa285b77162f8618991b96
annakhv <annakhvedelidze@gmail.com>
2023-05-20 20:45:11 +0400

    adds consoleMode failing tests

--- src/main/java/com/messenger/mode/ConsoleMode.java
+++ src/main/java/com/messenger/mode/ConsoleMode.java
@@ -1,15 +1,13 @@
 package com.messenger.mode;
 
-import java.util.Map;
-
 public class ConsoleMode extends Mode{
 
     public static ConsoleMode getConsoleModeInstance(){
         return new ConsoleMode();
     }
 
-    public String readConsoleInput(){
-        return null;
+    public String readConsoleInput()  {
+      return null;
     }
 
     public void writeConsoleOutput(String output){
