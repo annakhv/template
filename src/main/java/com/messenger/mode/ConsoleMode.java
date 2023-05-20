8a15eef31de088c3e4458d59d824e777b3425a84
annakhv <annakhvedelidze@gmail.com>
2023-05-20 20:51:44 +0400

    consoleMode tests pass

--- src/main/java/com/messenger/mode/ConsoleMode.java
+++ src/main/java/com/messenger/mode/ConsoleMode.java
@@ -1,15 +1,34 @@
 package com.messenger.mode;
 
-public class ConsoleMode extends Mode{
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-    public static ConsoleMode getConsoleModeInstance(){
+public class ConsoleMode extends Mode {
+
+    public static ConsoleMode getConsoleModeInstance() {
         return new ConsoleMode();
     }
 
-    public String readConsoleInput()  {
-      return null;
+    public String readConsoleInput() {
+        StringBuilder stringBuilder = new StringBuilder();
+        try {
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(System.in));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                stringBuilder.append(line).append("\n");
+            }
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return stringBuilder.toString();
     }
 
-    public void writeConsoleOutput(String output){
+    public void writeConsoleOutput(String output) {
+        System.out.print(output);
     }
 }
